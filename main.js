7c55cb6baaea441e9dadf3fe756bb35a7d9304a2
Wonil Heo <wonza4372@naver.com>
2021-12-09 20:40:44 +0900

    JS: randomize position
    randomize the position of items

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
 "use strict";
 
 const startBtn = document.querySelector(".start");
-const carrot = document.querySelector(".carrot");
 const btmSect = document.querySelector(".bottom__section");
 
 startBtn.addEventListener("click", () => {
-  console.log(btmSect.getBoundingClientRect());
-  createBug();
+  for (let i = 0; i < 8; i++) {
+    btmSect.appendChild(createCarrot(i));
+    btmSect.appendChild(createBug(i));
+  }
 });
 
-function createBug() {
-  btmSect.innerHTML = `
-  <img class="bug" id="bug1" src="img/bug.png" alt="bug" />
-  `;
+function createBug(bugNum) {
+  let bug = document.createElement("img");
+  bug.setAttribute("class", "bug");
+  bug.setAttribute("data-id", bugNum);
+  bug.setAttribute("src", "img/bug.png");
+
   const randomWidth = Math.random() * btmSect.getBoundingClientRect().width;
   const randomHeight = Math.random() * btmSect.getBoundingClientRect().height;
-  const bug = document.querySelector("#bug1");
+
   bug.style.left = `${randomWidth}px`;
   bug.style.top = `${randomHeight}px`;
+  return bug;
 }
 
-// for (i = 0; i <= 10; i++) {
+function createCarrot(carrotNum) {
+  let carrot = document.createElement("img");
+  carrot.setAttribute("class", "carrot");
+  carrot.setAttribute("data-id", carrotNum);
+  carrot.setAttribute("src", "img/carrot.png");
 
-// }
+  const randomWidth = Math.random() * btmSect.getBoundingClientRect().width;
+  const randomHeight = Math.random() * btmSect.getBoundingClientRect().height;
+
+  carrot.style.left = `${randomWidth}px`;
+  carrot.style.top = `${randomHeight}px`;
+  return carrot;
+}
