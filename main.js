c3ccef35b0aa69537c629f18ae8e0f70eef41f21
Wonil Heo <wonza4372@naver.com>
2021-12-12 00:09:52 +0900

    JS: resolve errors
    resolve errors by declaring a count variable in initSetting function

--- main.js
+++ main.js
@@ -6,23 +6,32 @@ const countNum = document.querySelector(".count");
 const btmSect = document.querySelector(".bottom__section");
 
 // Initiate the game setting
-let initSum = Math.ceil(Math.random() * 10 + 5);
-function initAssem(initSum) {
-  countNum.innerHTML = `${initSum}`;
-  for (let i = 0; i < initSum; i++) {
+// let count = Math.ceil(Math.random() * 5 + 5);
+function placement(count) {
+  countNum.innerHTML = `${count}`;
+  for (let i = 0; i < count; i++) {
     btmSect.appendChild(createCarrot(i));
     btmSect.appendChild(createBug(i));
   }
   const carrots = document.querySelectorAll(".carrot");
   return carrots;
 }
+function resetSetting() {
+  countNum.innerHTML = "?";
+  btmSect.innerHTML = "";
+  playIcon.classList.remove("fa-square");
+  playIcon.classList.add("fa-play");
+  let count = Math.ceil(Math.random() * 5 + 5);
+  return count;
+}
 
 // Start the game
 controlBtn.addEventListener("click", () => {
+  let count = Math.ceil(Math.random() * 5 + 5);
   playIcon.classList.remove("fa-play");
   playIcon.classList.add("fa-square");
-  const carrotsNode = initAssem(initSum);
-  removeCarrots(carrotsNode);
+  const carrotsNode = placement(count);
+  removeCarrots(carrotsNode, count);
   removeBugs();
 });
 
@@ -34,23 +43,26 @@ function removeBugs() {
       var bugPullAudio = new Audio("./sound/bug_pull.mp3");
       bugPullAudio.play();
       bug.classList.add("invisible");
+      resetSetting();
     });
   });
 }
 
 // Remove existing carrots
-function removeCarrots(carrotsNode) {
-  carrotsNode.forEach((carrot) => {
+function removeCarrots(carrots, count) {
+  carrots.forEach((carrot) => {
     carrot.addEventListener("click", () => {
       var carrotPullAudio = new Audio("./sound/carrot_pull.mp3");
       carrotPullAudio.play();
       carrot.classList.add("invisible");
-      initSum--;
-      countNum.innerHTML = `${initSum}`;
-      if (initSum == 0) {
+      count--;
+      countNum.innerHTML = `${count}`;
+      if (count == 0) {
         console.log("completed");
         var winAudio = new Audio("./sound/game_win.mp3");
         winAudio.play();
+        resetSetting();
+        // let count = Math.ceil(Math.random() * 5 + 5);
       }
     });
   });
@@ -60,7 +72,6 @@ function removeCarrots(carrotsNode) {
 function createBug(bugNum) {
   const bug = document.createElement("img");
   bug.setAttribute("class", "bug");
-  // bug.setAttribute("class", "invisible");
   bug.setAttribute("data-id", bugNum);
   bug.setAttribute("src", "img/bug.png");
 
@@ -78,13 +89,11 @@ function createBug(bugNum) {
 function createCarrot(carrotNum) {
   const carrot = document.createElement("img");
   carrot.setAttribute("class", "carrot");
-  // carrot.setAttribute("class", "invisible");
   carrot.setAttribute("data-id", carrotNum);
   carrot.setAttribute("src", "img/carrot.png");
 
   const randomWidth =
     Math.random() * (btmSect.getBoundingClientRect().width - carrot.width);
-  // console.log(btmSect.getBoundingClientRect().width - carrot.width);
   const randomHeight =
     Math.random() * (btmSect.getBoundingClientRect().height - carrot.height);
 
