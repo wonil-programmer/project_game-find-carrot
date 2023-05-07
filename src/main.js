f41b9039e675d8bda317b744cf0750b37bf0b5d5
Wonil Heo <wonza8585@gmail.com>
2023-05-07 23:20:59 +0900

    JS: add annotations

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 "use strict";
+// 모든 js파일들을 import
 import Field from "./field.js";
 import PopUp from "./popup.js";
 import Game from "./game.js";
 import * as sound from "./sound.js";
 
-// Make an object from Popup class. It contains the result of game
+// 게임의 결과를 포함하는 Popup 객체를 생성 후 gameFinishBanner 변수에 대입
 const gameFinishBanner = new PopUp();
+// 변수에 클릭리스너 세트
 gameFinishBanner.setClickListener(() => {
   game.initGame();
 });
 
+// 게임 실행과 관련된 Game 객체를 생성 후 game 변수에 대입 (게임 실행)
 const game = new Game();
+// reason에 따른 메시지와 bgm 지정 후 팝업 노출 (게임 결과)
 game.setGameStopListener((reason) => {
-  console.log(reason);
   let message;
   switch (reason) {
     case "cancel":
